cdd70b826bbeeae76a547d313e2c38b6ca788b9d
CodeExcalibar <Alex.raju1010@gmail.com>
2023-03-06 16:18:21 +0000

    added power tiles

--- project/script.js
+++ project/script.js
@@ -1,11 +1,28 @@
 const board = document.getElementById("board");
 let tile;
+let powerClass = ["triple-word", "double-word", "triple-letter", "double-letter"];
 
-function tripleWord(tile) {
-    console.log(tile)
+function powerTile(tile, text) {
+    let newText = document.createTextNode(text);    // Create power text
+    let p = document.createElement("p");            // Create paragraph element
+  
+    switch (text) {
+        case "Triple Word":
+            tile.classList.add(powerClass[0]);              // Adds class 'triple-word' to the tile
+            p.appendChild(newText);                         // Adds text to the paragraph
+            tile.appendChild(p);                            // Adds the paragraph to the tile
+            break;
+
+        case "Double Letter":
+            tile.classList.add(powerClass[3]);              // Adds class 'triple-word' to the tile
+            p.appendChild(newText);                         // Adds text to the paragraph
+            tile.appendChild(p);                            // Adds the paragraph to the tile
+            break;
+    }
+
 }
 
-
+console.log(new Date().getDay())
 
 // used for to count the number of tiles need 
 let count = 0;
@@ -18,12 +35,19 @@ for (let i = 0; i < 15; i++) {
         tile.classList.add("tile", `${count}`);
         board.append(tile);
 
+        // rendering the hpower tiles on specific location the board
+        if (count == 1 || count == 16 || count == 31|| count == 46|| count == 61|| count == 76|| count == 91|| count == 106 || count == 121 || count == 136 || count == 151 || count == 166 || count == 181 || count == 196 || count == 211) {
+        powerTile(tile, "Triple Word");
+        }  else if (count == 4) {
+            powerTile(tile, "Double Letter");
+        } else if (count == null) {
+            powerTile(count, "Double Word");
+        } else if (count == null) {
+            powerTile(tile, "Triple Letter");
+        }
     
     }
 }
-console.log(count)
-if (count = 1) {
-    tripleWord(tile)
-}
+
 
 
